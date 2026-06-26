e5100ad0d3c1560a3abefca0fa1e2a22f377b653
Deregowski <ped70qid@hhu.de>
2026-06-26 21:34:04 +0200

    clean push everything first

---
+++ tests/test_parser.py
@@ -0,0 +1,22 @@
+from proj.parser import * 
+from proj.parser import Parse 
+import pytest 
+
+SENT_LEN = 10
+
+parse = Parse(SENT_LEN)
+
+SHIFT = 0; RIGHT = 1; LEFT = 2;
+
+def test_transition_shift():
+    s = [1]
+    position = transition(move= SHIFT, i= 2, stack= s, parse=parse)
+    assert position == 3 
+    assert s == [1,2]
+
+def test_transition_right():
+    s = [1,2]
+    position = transition(move= RIGHT, i= 3, stack= s, parse=parse)
+    assert position == 3 
+    assert s == [1]
+    
